f95d5d0abc1ed5baa42f6db3fa8ecf8511a82f66
Ricky <binbing0525@163.com>
2017-05-08 11:13:13 +0800

    update demo

--- app/src/main/java/com/ricky/avantipro/MainActivity.java
+++ app/src/main/java/com/ricky/avantipro/MainActivity.java
@@ -1,51 +1,65 @@
 package com.ricky.avantipro;
 
 import android.content.Context;
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.view.View;
+import android.support.v7.widget.Toolbar;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.ricky.avanti.base.BaseBean;
+import com.ricky.avanti.base.SimpleActivity;
 import com.ricky.avanti.http.utils.RxSubscribe;
 import com.ricky.avanti.http.utils.RxUtils;
 import com.ricky.avantipro.bean.LoginBean;
 import com.ricky.avantipro.net.RickyHttpUtils;
 
+import butterknife.BindView;
+import butterknife.OnClick;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends SimpleActivity {
+
 
     private Context mContext;
 
+    @BindView(R.id.toolbar)
+    Toolbar mToolbar;
+    @BindView(R.id.btn_net_test)
+    Button testBtn;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public int getLayout() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    public void initEventAndData() {
         mContext = this;
-        findViewById(R.id.btn_net_test).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                RickyHttpUtils.getInstance(mContext)
-                        .login("Ricky", "123456")
-                        .compose(RxUtils.<BaseBean<LoginBean>>rxSchedulerHelper())
-                        .compose(RxUtils.<LoginBean>handleResult())
-                        .subscribe(new RxSubscribe<LoginBean>(mContext) {
-                            @Override
-                            public boolean showProgress() {
-                                return super.showProgress();
-                            }
 
-                            @Override
-                            public void _onNext(LoginBean loginBean) {
-                                Toast.makeText(mContext, "登录成功", Toast.LENGTH_SHORT).show();
-                            }
+        initToolbar(mToolbar);
+    }
 
-                            @Override
-                            public void _onError(int errorType, String message) {
-                                Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show();
-                            }
-                        });
-            }
-        });
+    @OnClick(R.id.btn_net_test)
+    public void testButtonClick() {
+        RickyHttpUtils.getInstance(mContext)
+                .login("Ricky", "123456")
+                .compose(RxUtils.<BaseBean<LoginBean>>rxSchedulerHelper())
+                .compose(RxUtils.<LoginBean>handleResult())
+                .subscribe(new RxSubscribe<LoginBean>(mContext) {
+                    @Override
+                    public boolean showProgress() {
+                        return super.showProgress();
+                    }
+
+                    @Override
+                    public void _onNext(LoginBean loginBean) {
+                        Toast.makeText(mContext, "登录成功", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void _onError(int errorType, String message) {
+                        Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show();
+                    }
+                });
+
     }
 }
